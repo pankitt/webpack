514bf289f30dc185b9fede67a56c8a7e5b4fb300
i.ustinchenko <delfa07@bigmir.net>
2019-02-13 11:22:47 +0200

    001

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './cantainers/App'
+import "./styles/scss/common.scss";
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const plugins = new HtmlWebPackPlugin({
+    template: "./public/index.html",
+    filename: "./index.html"
+});
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(jpe?g|png|gif|mp4|svg|eot|woff(2)?|ttf)$/,
+                loaders: ['file-loader']
+            }
+        ]
+    },
+    plugins: [plugins]
+};
