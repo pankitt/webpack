5d5319223d30756b9a857c33bef56fbd26e2e3e0
i.ustinchenko <delfa07@bigmir.net>
2019-02-13 11:48:10 +0200

    002

--- src/cantainers/App.js
+++ src/cantainers/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import reactLogo from '../styles/i/react.svg';
+import webpackLogo from '../styles/i/webpack.svg';
 
 class App extends Component{
     render() {
         return (
-            <div>
-                <h3>Test</h3>
+            <div className="one">
+                <h3>
+                    Test
+                    <img src={reactLogo} alt="logo" />
+                    <img src={webpackLogo} alt="logo" />
+                </h3>
             </div>
         )
     }
